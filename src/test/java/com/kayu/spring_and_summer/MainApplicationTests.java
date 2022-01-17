b256be6d16f4a65acf7dc76b37b809fc16e7dd72
muhammad-khair <muhammad_khair@outlook.com>
2022-01-17 21:52:41 +0800

    Add spring framework

---
+++ src/test/java/com/kayu/spring_and_summer/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayu.spring_and_summer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
